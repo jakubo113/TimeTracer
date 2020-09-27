327c84c5130e7e26478089bd9938b80bab0ed4a6
jakubo113 <69671604+jakubo113@users.noreply.github.com>
2020-09-27 14:38:19 +0200

    init commit

---
+++ android/app/src/main/kotlin/com/example/time_tracing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.time_tracing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class Activity {
+  String track;
+  DateTime timeStamp;
+
+  Activity({this.track, this.timeStamp});
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> tracks = [];
+  List<DateTime> timeStamp = [];
+
+  List<Activity> activities = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Time tracer"),
+      ),
+      body: getList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          activities.add(Activity(track: "", timeStamp: DateTime.now()));
+
+          // tracks = addToListElement("", tracks);
+          // timeStamp.add(DateTime.now());
+          debugPrint(tracks.toString());
+          setState(() {});
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  List<String> addToListElement(String text, List pole) {
+    pole.add(text);
+    return pole;
+  }
+
+  Widget getList() {
+    // var list = ListView.builder(
+    //     itemCount: activities.length,
+    //     itemBuilder: (BuildContext ctxt, int index) {
+    //       return ListTile(
+    //         title: Text(activities[index].timeStamp.toString()),
+    //         subtitle: Text(activities[index].track),
+    //         onTap: () {
+    //           debugPrint("klikam");
+    //           alertDialog(context);
+    //         },
+    //       );
+    //     });
+
+    List<ListTile> listItems = activities.map((activity) =>
+        ListTile(
+          title: Text(activity.timeStamp.toString()),
+          subtitle: Text(activity.track),
+          onTap: () {
+            debugPrint("klikam");
+            alertDialog(context);
+          },
+        )
+    ).toList();
+
+    return Column(
+      children: [
+        Text('Cas'),
+        Expanded(child: ListView(children: listItems,))
+      ],
+    );
+  }
+
+  void alertDialog(BuildContext context) {
+    var alDialog = AlertDialog(
+      title: Text("data"),
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alDialog;
+      },
+    );
+  }
+}
